e431d5cc4677a56569839b2a40a2c9f07b7627f7
MetalRoz <wilsonmiguel2003@gmail.com>
2022-10-27 20:26:52 -0300

    agregando modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header.component/header.component.component';
+import { AboutMeComponentComponent } from './about-me.component/about-me.component.component';
+import { ExperiencesComponentComponent } from './experiences.component/experiences.component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    AboutMeComponentComponent,
+    ExperiencesComponentComponent
   ],
   imports: [
     BrowserModule
